d949779bf40565bb2fe144048b15c104574f332b
RalphOng22 <ralph.ong.2022@scis.smu.edu.sg>
2024-09-23 18:18:39 +0800

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -15,9 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-# Create the tables if they don't exist
-with app.app_context():
-    db.create_all()
+
 CORS(app, supports_credentials=True)
 
 app.register_blueprint(main)
