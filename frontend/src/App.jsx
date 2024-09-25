c21a713e48ab2d8460116decf1cc97372be06e49
soapy26 <psmontalban.2022@scis.smu.edu.sg>
2024-09-25 16:20:14 +0800

    created navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
+import Sidebar from './components/sidebar/sidebar';
+import Navbar from './components/navbar/navbar';
+import { useLocation } from 'react-router-dom';
+import './App.css';  // Import the CSS file
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className="app-layout">
+        {/* <Navbar showDateSelector={showDateSelector} showSearch={showSearch} showProfile={showProfile} /> */}
+        <Navbar />
+        <Sidebar />  {/* Sidebar is included here, so it is rendered on every page */}
+      </div>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-      </div>
     </Router>
   );
 }
