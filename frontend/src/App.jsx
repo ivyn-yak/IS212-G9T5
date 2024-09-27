0c25fc2ba99c30d708b76cf1a2d2421022f3ecf1
chi nguyen <lccnguyen.2022A@scis.smu.edu.com>
2024-09-27 11:13:17 +0800

    schedule component centered
    i cried. some pls check if it looks good in a different viewport dimension, before merging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,21 @@ import StaffViewSchedule from './pages/StaffViewSchedule';
 import Sidebar from './components/sidebar/sidebar';
 import Navbar from './components/navbar/navbar';
 import { useLocation } from 'react-router-dom';
-import './App.css';  // Import the CSS file
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="app-layout">
         <Navbar />
-        <Sidebar />
-        <div className="content-area">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/staff/view-schedule" element={<StaffViewSchedule />} />
-          </Routes>
+        <div className="main-container">
+          <Sidebar />
+          <div className="content-area">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/staff/view-schedule" element={<StaffViewSchedule />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </Router>
