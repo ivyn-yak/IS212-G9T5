e39a25674ba5f09031470ede8c4fcda8d745eebd
chi nguyen <lccnguyen.2022A@scis.smu.edu.com>
2024-11-01 08:06:30 +0800

    fix config files

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -2,7 +2,6 @@ const config = {
   ENDPOINT_BE_URL: import.meta.env.VITE_ENDPOINT_BE_URL
 };
 
-// Log configuration in development
 if (import.meta.env.DEV) {
   console.log('Current environment:', import.meta.env.MODE);
   console.log('API endpoint:', config.ENDPOINT_BE_URL);
